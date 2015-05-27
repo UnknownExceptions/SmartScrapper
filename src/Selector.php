7bbaf72efc0d29ab241d1a56733c2974a13efa81
Henrique Dias <hacdias@gmail.com>
2015-05-27 07:22:31 +0100

    Update Selector.php

--- src/Selector.php
+++ src/Selector.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the WebParser package.
+ * This file is part of the SmartScraper package.
  *
  * (c) Henrique Dias <hacdias@gmail.com>
  * (c) Luís Soares <lsoares@gmail.com>
@@ -9,7 +9,7 @@
  * Licensed under the MIT license.
  */
 
-namespace SmartScrapper;
+namespace SmartScraper;
 
 /**
  * Selector
